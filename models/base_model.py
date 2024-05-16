ce1c9be09f3ec6d65fce95c12e688529a9e9334c
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-05-16 21:16:40 +0100

    docs review expanded

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines an class variable called BaseModel, and will be
+the base class of all the other classes in this project.
+
+class
+======
+Basemodel
+
+
+Methods
+==========
+
+...
+
+Attributes
+==========
+
+...
+
+"""
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    """ defines all common attributes/methods for other classes: """
 
     def __init__(self, *args, **kwargs) -> None:
+        """ takes in all multiple parameters
+
+        args:
+            args: multiple none keyword argurments
+            kwargs: multiple keywor arguments
+        """
         time_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for k, v in kwargs.items():
@@ -20,13 +48,18 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self) -> str:
+        """ Returns <class name> <self.id> <self.__dict__> """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self) -> None:
+        """ updates the public instance attribute updated_at """
+
         '''this method save data to a json file'''
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/value of __dict__
+        of instance """
         json = self.__dict__
         json['__class__'] = self.__class__.__name__
         json['created_at'] = json['created_at'].isoformat()
